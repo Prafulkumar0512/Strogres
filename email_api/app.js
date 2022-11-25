03f2ccf6efeaa1fbc33a4b736c7d89b44f4ec567
Praful kumar <aruparayilprpon19ce@student.mes.ac.in>
2022-11-25 18:30:13 +0530

    some changes

---
+++ email_api/app.js
@@ -0,0 +1,64 @@
+const fetch = require('node-fetch')
+var express = require('express');
+const sgMail = require('@sendgrid/mail')
+const dotenv = require('dotenv')
+// const cors = require('cors')
+dotenv.config()
+var app = express();
+const textract = require('textract');
+var bodyParser = require('body-parser');
+app.set('port', process.env.PORT || 6000)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/extract', (req, res) => {
+    const data = req.body;
+    console.log("running api")
+    textract.fromUrl(data.url, async (err, text) => {
+        if (err) {
+            res.status(404).send(err)
+        }
+        else {
+            console.log(text)
+            try {
+                const response = await fetch('http://127.0.0.1:5000/extractData', {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        data: text
+                    })
+                })
+                const resData = await response.json()
+                res.send(resData)
+            }
+            catch (err) {
+                res.send({ error: err.message })
+            }
+
+        }
+    })
+
+})
+app.post('/sendMail', (req, res, next) => {
+    const data = req.body
+    console.log(process.env.SENDGRID_API_KEY)
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+        to: data.mail,
+        from: 'deepbluerpa@gmail.com',
+        subject: 'Application Selected',
+        html: `<h3>Hello ${data.name}</h3><br/><p>Your application has been selected</p><br/><p>Further details related to interviews will be shared soon</p>`,
+    }
+    sgMail
+        .send(msg)
+        .then(() => {
+            res.send('Email sent')
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
+})
+
+app.listen(app.get('port'), () => { console.log("Connected") })
+
